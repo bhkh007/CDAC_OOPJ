b9059d9b5969917bb910ace364d54cd8d1bb0bca
bhkh007 <khandarbhavesh007@gmail.com>
2024-03-26 16:30:28 +0530

    Created Assignment 9 and solved question 01

---
+++ CDAC_OOPJ/src/Assignment_9/Ques_01.java
@@ -0,0 +1,27 @@
+package Assignment_9;
+
+import java.util.Scanner;
+
+public class Ques_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		try {
+		System.out.println("Enter num1 : ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter num2 : ");
+		int num2 = sc.nextInt();
+		int result = num1 / num2;
+		System.out.println("Result : " + result);
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Exception found : "+ e.getMessage());
+		}
+		finally {
+			System.out.println("Finally statement");
+		}
+		
+	}
+
+}
